aa8db2b36443b9971aea040c7f116c77f5820392
wit <witwitchayakarn@gmail.com>
2023-03-22 14:44:56 +0700

    Add Paddle tests

--- js/iap.js
+++ js/iap.js
@@ -1,8 +1,9 @@
 const url = 'http://localhost:8088/'; //'https://iap-001.uc.r.appspot.com/';
+//const url = 'https://20230321t122323-dot-iap-001.uc.r.appspot.com/';
 
 const postVerify = async () => {
   const verifyUrl = url + 'verify';
-  const data = verifyReq2;
+  const data = verifyReq4;
 
   const response = await fetch(verifyUrl, {
     method: 'POST',
@@ -19,7 +20,7 @@ const postVerify = async () => {
 
 const postNotify = async (notifyPath) => {
   const notifyUrl = url + notifyPath;
-  const data = notifyReq1;
+  const data = notifyReq8;
 
   const response = await fetch(notifyUrl, {
     method: 'POST',
@@ -36,7 +37,7 @@ const postNotify = async (notifyPath) => {
 
 const postStatus = async () => {
   const statusUrl = url + 'status';
-  const data = statusReq1;
+  const data = statusReq3;
 
   const response = await fetch(statusUrl, {
     method: 'POST',
@@ -51,10 +52,32 @@ const postStatus = async () => {
   console.log(result);
 };
 
+const postPaddlePre = async () => {
+  const preUrl = url + 'paddle/pre';
+  const data = {
+    userId: '02d4bcea7320a06c04026de2e063e7356bf46c54b34a691b839c3da53a60553fb9',
+    randomId: '37bn-RW4G-oTUm-e4ni',
+  };
+
+  const response = await fetch(preUrl, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+  const result = response.status;
+  console.log(result);
+};
+
 const main = () => {
   //postVerify();
   //postNotify('playstore/notify');
   //postNotify('appstore/notify');
-  //postStatus();
+  //postNotify('paddle/notify');
+  postStatus();
+  //postPaddlePre();
 }
 main();
